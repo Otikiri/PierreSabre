20d903fde7637e315f3987f1e44a3926e045de91
Otikiri <136935486+Otikiri@users.noreply.github.com>
2024-10-21 18:13:19 +0200

    finished page 4

---
+++ src/personnages/Humain.java
@@ -0,0 +1,48 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonfav;
+	private int argent;
+
+	public Humain(String nom, String boissonfav, int argent) {
+		this.nom = nom;
+		this.boissonfav = boissonfav;
+		this.argent = argent;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getBoissonfav() {
+		return boissonfav;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("("+this.nom+") - "+texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour! Je m'appelle "+this.nom+"et j'aime boire du "+this.boissonfav);
+	}
+	
+	public void acheter(String bien,int prix) {
+		if (argent < prix) {
+			parler("Je n'ai pas assez de sous.");	
+		}
+		argent -= prix; 
+		parler("Jai acheter "+bien+" et maintenant il me reste "+argent+" sous");
+	}
+	
+	public void gagnerArgent(int gain) {
+		argent += gain; 
+	}
+	
+	public void perteArgent(int perte) {
+		argent -= perte; 
+	}
+	
+}
+
+
---
+++ src/scenario/Histoire4.java
@@ -0,0 +1,10 @@
+package scenario;
+
+import personnages.Humain;
+
+public class Histoire4 {
+	public static void main() {
+		Humain prof = new Humain("Prof","Kombucha",52);
+		prof.direBonjour();
+	}
+}
